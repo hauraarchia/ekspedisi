6eb237bad57412cb7aa8e6d2ffdb39c7f28c5549
isnaenytrilarassati <isnaenytrilarassati>
2023-09-13 14:23:19 +0700

    modifikasi isna

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -6,5 +6,6 @@ public class OurFirstGithubCollab {
     public static void main(String[] args ){
         System.out.println("ini fitur pertama sudah berjalan baik");
         System.out.println("ini fitur kedua sudah berjalan baik");
+        System.out.println("tambahan dari isna");
     }
 }
