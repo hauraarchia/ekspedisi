2e282821c4e9a39f7b573377e7fbb69de408c480
luthfimahardika <lutfi.mahardika17@gmail.com>
2023-10-19 07:26:30 +0700

    cek

--- tracking.java
+++ tracking.java
@@ -3,29 +3,41 @@ public class tracking {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String noResi, namaPenerima, status;
+        int barangSampai, tanggal, jam;
 
         System.out.println("Masukkan nomor resi(123A/234B/345C) :");
         noResi = sc.nextLine();
         switch(noResi){
             case "123A":
             System.out.println("No Resi :" +noResi);
-            System.out.println("Nama Penerima: Arifin");
-            System.out.println("Status: Terkirim");
+            System.out.println("Nama Penerima: Isnaeny");
             break;
             case "234B":
             System.out.println("No Resi :" +noResi);
-            System.out.println("Nama Penerima: Abinaya");
-            System.out.println("Status: On Process");
+            System.out.println("Nama Penerima: Lutfi");
             break;
             case "345C":
              System.out.println("No Resi :" +noResi);
             System.out.println("Nama Penerima: Haura");
-            System.out.println("Status: Dalam Pengiriman");
             break;
             default:
             System.out.println("maaf nomor resi yang anda masukkan tidak diketahui");
             break;
         }
 
+        System.out.println("Masukkan tanggal pemesanan");
+        tanggal = sc.nextInt();
+        System.out.println("Masukkan jam pemesanan");
+        jam = sc.nextInt();
+
+        if (jam <=12.00 ) {
+            System.out.println ("Pesanan dikirim hari ini");
+            System.out.println("Status : Terkirim");      
+         } else{
+            System.out.println("Barang dikirim besok");
+            System.out.println("Status : On process");}
+
+        barangSampai = (int) (tanggal + 4);
+        System.out.println("Estimasi barang sampai tanggal : " + barangSampai);
     }
 }
